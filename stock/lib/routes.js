9feaa04f4f3ce9686b9eef43c5e7088a66634c10
harryenlaweb <harryenlaweb@hotmail.com>
2019-08-31 13:54:09 -0300

    Video 06

--- stock/lib/routes.js
+++ stock/lib/routes.js
@@ -1,6 +1,19 @@
 import { Router } from 'meteor/iron:router';
 
 
+Router.onBeforeAction(function () {  
+
+  if (!Meteor.userId()) {    
+    Router.go('home');    
+  } else {
+    if(Router.current().route.getName() === 'home'){
+       Router.go('projects');       
+    }        
+  }
+  this.next();
+  
+});
+
 Router.configure({
 	layoutTemplate: 'baseLayout'
 	
